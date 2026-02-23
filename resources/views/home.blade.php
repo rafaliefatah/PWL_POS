87434b82da3f6713f9b5c110e10f5966b1df2f9c
rafaliefatah <rafazian336@gmail.com>
2026-02-24 01:22:50 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <center>
+            <h1>Website PWL POS<br>Rafazian Alief Fatah</h1>
+        </center>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/product', [ProductController::class, 'index']);
