d4ca2f411024945a9ee3ed385145858f81cd2352
rafaliefatah <rafazian336@gmail.com>
2026-02-24 14:03:05 +0700

    JS3_Praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/product', [ProductController::class, 'index']);
+
+Route::get('/level', [LevelController::class, 'index']);
