c7c9b11fccb6987df382be2f9c28695b0142809b
rafaliefatah <rafazian336@gmail.com>
2026-03-03 03:50:03 +0700

    JS4_Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,16 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function param($id, $name)
-    {
-        return view('user', compact('id', 'name'));
-    }
-
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort (404);
-        });
-        return view('userNew', ['data' => $user]);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,10 +1,22 @@
 <html>
     <head>
-        <title>User</title>
+        <title>Data User</title>
     </head>
     <body>
         <h1>Data User</h1>
-        <td>ID: {{ $id }}</td><br>
-        <td>Nama: {{ $name }}</td>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+            </tr>
+        </table>
     </body>
 </html>
